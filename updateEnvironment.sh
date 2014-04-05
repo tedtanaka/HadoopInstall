4468291f9875990b04f216c64870ae787ce69d7a
Ted Tanaka <ted@tedtanaka.com>
2014-04-05 18:13:12 -0400

    updated script

--- updateEnvironment.sh
+++ updateEnvironment.sh
@@ -2,4 +2,4 @@
 
 echo "export HADOOP_HOME=/opt/hadoop" >> /home/hduser/.bashrc
 echo "export PATH=\$PATH:\$HADOOP_HOME/bin" >> /home/hduser/.bashrc
-
+echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> /home/hduser/.bashrc
