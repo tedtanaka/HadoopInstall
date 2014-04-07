3f0544e86a440c725b3ca4c56847211d39074897
Ted Tanaka <ted@tedtanaka.com>
2014-04-06 23:12:05 -0400

    updated scripts

--- startHadoop.sh
+++ startHadoop.sh
@@ -8,3 +8,6 @@ $HADOOP_HOME/sbin/hadoop-daemon.sh start secondarynamenode
 $HADOOP_HOME/sbin/yarn-daemon.sh start resourcemanager
 $HADOOP_HOME/sbin/yarn-daemon.sh start nodemanager
 $HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
+
+# show Java processes
+jps
